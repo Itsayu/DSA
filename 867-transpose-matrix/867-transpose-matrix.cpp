45ee392791efaba9f9431f7fd607b5d9fe74992c
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-04 11:11:21 +0530

    Time: 16 ms (61.34%), Space: 10.7 MB (41.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int r=matrix[0].size();
-        vector<vector<int>> ayu(r, vector<int>(n,0));  //copied
-        for(int i=0; i<n; i++){
-            for(int j=0; j<r; j++){
+        int a=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> ayu(n, vector<int>(a,0));  //copied
+        for(int i=0; i<a; i++){
+            for(int j=0; j<n; j++){
                 ayu[j][i]= matrix[i][j];
             }
         }
