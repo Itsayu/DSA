ed936b556383a62d2962ecbc149b9d27b921e8cd
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-06 03:11:57 +0530

    Time: 4 ms (39.93%), Space: 8.6 MB (85.57%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i=n-1; i>=0; i--){
-			if(digits[i]<9) 
-            {
+        int ayu=digits.size();
+        for(int i=ayu-1; i>=0; i--){
+			if(digits[i]<9){
                 digits[i]++;
                 break;
             }
-            else
-            {
+            else{
                 digits[i] = 0;
             }
         }
