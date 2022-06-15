2ab632828cffbf228f7b2e99a9ed41d1830a4ec5
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-16 00:17:38 +0530

    Added solution - LeetHub

--- Smallest distinct window - GFG/smallest-distinct-window.cpp
+++ Smallest distinct window - GFG/smallest-distinct-window.cpp
@@ -9,22 +9,16 @@ class Solution{
     int findSubString(string str)
     {
         // Your code goes here 
-        int n = str.size();
-        
-        int uniq =  unordered_set<char>(str.begin(), str.end()).size();
-
-        unordered_map<char, int> hm;
-        
-        int i = 0, j = 0;
-        int ans = INT_MAX, count = INT_MAX;
-       
-        while(j < n){
- 
+        int n=str.size();
+        int uniq=unordered_set<char>(str.begin(), str.end()).size();
+        unordered_map<char, int>hm;
+        int i=0,j=0;
+        int ans=INT_MAX,count=INT_MAX;
+        while(j<n){
             hm[str[j++]]++;
-
-            while(hm.size() == uniq){
-                count = j-i;
-                ans = min(ans, count);
+            while(hm.size()==uniq){
+                count=j-i;
+                ans=min(ans,count);
                 hm[str[i]]--;
                 if(hm[str[i]]==0) 
                 	hm.erase(str[i]);
