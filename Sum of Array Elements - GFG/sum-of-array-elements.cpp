d9cb2b51b21ec11aa38939e8c1c17dd2884a8f44
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-21 00:34:39 +0530

    Added solution - LeetHub

---
+++ Sum of Array Elements - GFG/sum-of-array-elements.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+
+int sumElement(int arr[],int n)
+{
+    //Your code here
+     int ayu=0;
+        for(int i=0; i<n; i++){
+            ayu+=arr[i];
+        }
+        return ayu;
+}
+
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    cout<<sumElement(arr,n)<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
