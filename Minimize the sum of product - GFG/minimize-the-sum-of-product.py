68bcb6666e455b9ca6dc1beb456c811e8b04f798
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-21 09:28:26 +0530

    Added solution - LeetHub

---
+++ Minimize the sum of product - GFG/minimize-the-sum-of-product.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def minValue(self, a, b, n):
+        # Your code goes here
+        ayu=0
+        a.sort()
+        b.sort()
+        for i in range(0,n):
+            ayu+=a[i]*b[i]
+        return ayu
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        print(ob.minValue(a, b, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
