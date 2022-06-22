3572aa8473395dd6f4d31f5a2a8a92e32cfa71e1
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-22 08:48:01 +0530

    Added solution - LeetHub

--- Array Partition - GFG/array-partition.cpp
+++ Array Partition - GFG/array-partition.cpp
@@ -37,14 +37,14 @@ class Solution{
         // }
         // return dp[0];
         // --------------------------OR---------------------------
-        bool dp[N+1] = {0};
-        dp[0] = 1;
+        bool dp[N+1]={0};
+        dp[0]=1;
         sort(A.begin(),A.end());
-        for(int i=K; i<=N; i]++){
-            int l = lower_bound(A.begin(), A.end(), A[i-1] - M) - A.begin();
-            int h = i - K;
-            for(int j=l; j<=h; j++){
-                dp[i] |= dp[j];
+        for(int i=K;i<=N;i++){
+            int l=lower_bound(A.begin(),A.end(),A[i-1]-M)-A.begin();
+            int h=i-K;
+            for(int j=l;j<=h;j++){
+                dp[i]|=dp[j];
                 if (dp[i])
                     break;
             }
