0dc4de0fb6b93e80b6dc71808934af0307c9be03
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-27 02:06:05 +0530

    Added solution - LeetHub

---
+++ Change Bits - GFG/change-bits.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        // code here
+        int K=N;
+        int cnt=0;
+        while(N>0)
+        {
+            cnt++;
+            N=N/2;
+        }
+        int a=pow(2,cnt)-1;
+        int b=a-K;
+        return {b,a};
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
