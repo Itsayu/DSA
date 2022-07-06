7af2b7d8b3afc9c1ee843c4683d0822879b63f56
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-07-06 08:38:59 +0530

    Time: 22 ms (16.18%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1)                                     // if n == 0 or n == 1 
-          return n ;                                // return number 
-        int output = fib(n-1)+fib(n-2);            // used recursion to call the function   
-        return output ;
+        if(n<=1)   
+          return n ;  
+        int ayu=fib(n-1)+fib(n-2);             
+        return ayu;
     }
 };
