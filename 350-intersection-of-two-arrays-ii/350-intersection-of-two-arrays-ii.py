1121fca89c80009c28902c3895195a91f35b8dd0
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-07-17 22:53:10 +0530

    Time: 44 ms (98.30%), Space: 14 MB (85.59%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -3,7 +3,7 @@ class Solution:
         nr=Counter(nums1)
         result=[]
         for n in nums2:
-            if nr[n] > 0:
+            if nr[n]>0:
                 result.append(n)
-                nr[n] -= 1
+                nr[n]-=1
         return result
