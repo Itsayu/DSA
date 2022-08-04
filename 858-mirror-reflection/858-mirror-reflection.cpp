ef1bd3a0092bff7c62d5c3e1050e71e2ac323f0b
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-08-04 07:06:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        int height = q;
-        int new_height;
-        bool reflections = 0;
-        
-        int bounce = 1;    // 1 = up, -1 = down
-        
-        while (height != 0 && height != p) {
-            reflections = !reflections;  // switch even / odd count of reflections
-            new_height = height + (q * bounce);
-            if (new_height > p) {
-                new_height = p - (q - (p - height));
-                bounce = -bounce;
-            }
-            else if (new_height < 0) {
-                new_height = 0 + (q - height);
-                bounce = -bounce;
-            }
-            height = new_height;
+         while(p % 2 == 0 && q % 2 == 0){
+            p = p >> 1;
+            q = q >> 1;
         }
-        
-        if (height == 0) {
-            return 0;
-        }
-        return (reflections) ? 2 : 1;
+        if(p % 2 == 1) return q % 2;
+        return 2;
     }
 };
