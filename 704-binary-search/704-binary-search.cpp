8ed01491a7211d72b7b85fdc3bedb36f09753cf8
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-08-15 01:33:15 +0530

    Time: 57 ms (55.06%), Space: 27.5 MB (92.17%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         // for(int i=0; i<sizeof(nums)-1; i++){
+//         for(int i=0; i<nums.size()-1; i++){
+//             if(nums[i]==target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target){
+        
+        int n = nums.size()-1;
+        int low = 0;
+        int high = n;
+        
+        while (low <= high){
+            // int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
+            
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+};
