30598846d4b3193f139d29a0aa783fbe1788a90d
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-09-07 01:28:48 +0530

    Time: 28 ms (11.16%), Space: 5.9 MB (40.20%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)   
-          return n ;  
-        int ayu=fib(n-1)+fib(n-2);             
-        return ayu;
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 };
