afff1d8a03729a9b6bef7bc414de34f5721f4af2
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-09-29 08:52:13 +0530

    Time: 56 ms (85.20%), Space: 30.9 MB (98.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> p;
-        for(auto i:arr){
-            p.push(make_pair(abs(i-x),i));
-        }
-        vector<int> ans;
-        while(k--){
-            ans.push_back(p.top().second);
-            p.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> p;
+        // for(auto i:arr){
+        //     p.push(make_pair(abs(i-x),i));
+        // }
+        // vector<int> ans;
+        // while(k--){
+        //     ans.push_back(p.top().second);
+        //     p.pop();
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
+        
+        int l = 0;
+       int r = arr.size() - k;
+
+       while (l < r) {
+         const int m = (l + r) / 2;
+         if (x - arr[m] <= arr[m + k] - x)
+           r = m;
+         else
+           l = m + 1;
+       }
+
+       return {begin(arr) + l, begin(arr) + l + k};
     }
 };
