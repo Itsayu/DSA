e1e5e3ce1b64eb802ee46df5cdf651c67f93e4ec
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-12-26 19:30:41 +0530

    Update 0055-jump-game.cpp

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,7 +3,8 @@ public:
     bool canJump(vector<int>& nums) {
         int n=nums.size(), canReach=0;
         for(int i=0;i<n;i++){
-            if(canReach<i)return 0;
+            if(canReach<i)
+                return 0;
             canReach=max(canReach,i+nums[i]);
         }
         return 1;   
