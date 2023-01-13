04544690f3a49e995eeb965c58c3f4aa1dda1f29
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-01-13 15:38:20 +0530

    Time: 83 ms (65.94%), Space: 33.7 MB (54.21%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> ayu;
+        vector<int> nr;
         for(int i=0; i<nums.size()-1; i++){
             if(nums[i]==nums[i+1]){
-                ayu.push_back(nums[i]);
+                nr.push_back(nums[i]);
             }
         }
-        return ayu;
+        return nr;
     }
 };
