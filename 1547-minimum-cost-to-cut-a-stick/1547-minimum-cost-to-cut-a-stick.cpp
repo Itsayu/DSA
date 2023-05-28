5494fe828f73b86863c58d8886455ffc2388d7ba
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-05-28 08:38:19 +0530

    Time: 133 ms (14.21%), Space: 10.3 MB (32.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -4,10 +4,10 @@ public:
         sort(cuts.begin(),cuts.end());
         cuts.insert(cuts.begin(),0);
         cuts.push_back(l);int n=cuts.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,9999999));
+        vector<vector<int>> nr(n+1,vector<int>(n+1,9999999));
         for(int i=0;i<n-1;i++)
         {
-            dp[i][i]=0;
+            nr[i][i]=0;
         }
         for(int k=2;k<=n;k++)
         {
@@ -17,10 +17,10 @@ public:
                 for(int d=i;d<j;d++)
                 {   
                     
-                    dp[i][j]=min(dp[i][j],dp[i][d]+dp[d+1][j]+cuts[j+1]-cuts[i]);
+                    nr[i][j]=min(nr[i][j],nr[i][d]+nr[d+1][j]+cuts[j+1]-cuts[i]);
                 }
             }
         }
-        return dp[0][n-2];
+        return nr[0][n-2];
     }
 };
